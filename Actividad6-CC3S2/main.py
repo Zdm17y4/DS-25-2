e38f5c45bc5b990eef64f981d780ff1e5257fa60
Zdm17y4 <miguelangel20010817@gmail.com>
2025-09-26 15:56:54 -0500

    Agregar el cherry-pick hecho en main.py

--- Actividad6-CC3S2/main.py
+++ Actividad6-CC3S2/main.py
@@ -1,1 +1,7 @@
 print('Hello World-actualizado en main')
+def greet():
+	print('Hello como una función avanzada')
+greet()
+def greet():
+	print('Error corregido en la función')
+print('Cherry pick')
